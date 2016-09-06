725b1658e04cf9bbf580e5281c97eb89e23e36f9
pelgee <pelgee@terpmail.umd.edu>
2016-09-06 16:17:27 -0400

    Update aom.h
    adding saving of previous VCO, attenuation values

--- arduino/simple-aom/aom.h
+++ arduino/simple-aom/aom.h
@@ -70,6 +70,8 @@ public:
 
   uint16_t pow_to_dac(double power);
   double dac_to_pow(uint16_t dacVal);
+  float attinit, vcoinit;
+  int eepromvco_ptr;
 
 private:
   // whether to display voltage or calibration units
@@ -98,46 +100,54 @@ void AOM::init(){
   digitalWrite(DAC_LDAC, LOW);
 
   // pull in old AOM state checkpoint; program default values if necessary
-  EEPROM.get(0, checkpoint);
-  if(checkpoint.factory_default != FACTORY_DEFAULT_EEPROM){
-    Serial.println("Factory default EEPROM detected; reprogramming");
-    checkpoint.factory_default = FACTORY_DEFAULT_EEPROM;
-    checkpoint.dac0 = 0;
-    checkpoint.dac1 = 0;
-    EEPROM.put(0, checkpoint);
-  }
+  //EEPROM.get(0, checkpoint);
+ // if(checkpoint.factory_default != FACTORY_DEFAULT_EEPROM){
+ //   Serial.println("Factory default EEPROM detected; reprogramming");
+  //  checkpoint.factory_default = FACTORY_DEFAULT_EEPROM;
+//checkpoint.dac0 = 0;
+ //   checkpoint.dac1 = 0;
+ //   EEPROM.put(0, checkpoint);
+ // }
 
   // increment to find calibration EEPROM address;
   // bootstraps for startup
-  int eeprom_ptr = sizeof(checkpoint);
+ // int eeprom_ptr = sizeof(checkpoint);
 
-  EEPROM.get(eeprom_ptr, _freq_calib);
-  if (_freq_calib.address != eeprom_ptr){
-    Serial.println("Factory default frequency calibration");
-    _freq_calib.address = eeprom_ptr;
-    _freq_calib.valid = false;
-    _freq_calib.a = 0.0;
-    _freq_calib.b = 0.0;
-    _freq_calib.c = 0.0;
-    _freq_calib.d = 0.0;
-    EEPROM.put(eeprom_ptr, _freq_calib);
-  }
+  //EEPROM.get(eeprom_ptr, _freq_calib);
+  //if (_freq_calib.address != eeprom_ptr){
+  //  Serial.println("Factory default frequency calibration");
+  //  _freq_calib.address = eeprom_ptr;
+  //  _freq_calib.valid = false;
+  //  _freq_calib.a = 0.0;
+  //  _freq_calib.b = 0.0;
+  //  _freq_calib.c = 0.0;
+  //  _freq_calib.d = 0.0;
+  //  EEPROM.put(eeprom_ptr, _freq_calib);
+  //}
 
   // increment and get power calibration
-  eeprom_ptr += sizeof(_freq_calib);
-  EEPROM.get(eeprom_ptr, _pow_calib);
-  if (_pow_calib.address != eeprom_ptr){
-    Serial.println("Factory default power calibration");
-    _pow_calib.address = eeprom_ptr;
-    _pow_calib.valid = false;
-    _pow_calib.a = 0.0;
-    _pow_calib.b = 0.0;
-    _pow_calib.c = 0.0;
-    _pow_calib.d = 0.0;
-    EEPROM.put(eeprom_ptr, _pow_calib);
+  //eeprom_ptr += sizeof(_freq_calib);
+  //EEPROM.get(eeprom_ptr, _pow_calib);
+  //if (_pow_calib.address != eeprom_ptr){
+  //  Serial.println("Factory default power calibration");
+  //  _pow_calib.address = eeprom_ptr;
+  //  _pow_calib.valid = false;
+  //  _pow_calib.a = 0.0;
+  //  _pow_calib.b = 0.0;
+  //  _pow_calib.c = 0.0;
+  //  _pow_calib.d = 0.0;
+  //  EEPROM.put(eeprom_ptr, _pow_calib);
+  //}
+  
+  EEPROM.get(0,attinit);
+  eepromvco_ptr = sizeof(attinit);
+  EEPROM.get(eepromvco_ptr,vcoinit);
+  if(attinit != attinit){
+    attinit = 0;
   }
-
-
+  if(vcoinit != vcoinit){
+    vcoinit = 0;
+  }
 
   delay(1);
 
